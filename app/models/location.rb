4b620b12b2a167ff5e8a2cb7beec0e2a625a16c7
Vaughn <vaughn.weiss@gmail.com>
2020-12-16 13:28:19 -0700

    Scopes // Add subclass and visited scopes for Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,19 @@
 class Location < ApplicationRecord
+  # validations
   validates :type, presence: true
   validates :name, presence: true
+
+  # constants
+  TYPES = [
+    'dungeon',
+    'geography',
+    'municipality'
+  ]
+
+  # scopes
+  TYPES.each do |type|
+    scope type.pluralize.to_sym, -> { where(type: type.capitalize) }
+  end
+  scope :visited, -> { where(visited: true) }
+  scope :unvisited, -> { where(visited: false) }
 end
