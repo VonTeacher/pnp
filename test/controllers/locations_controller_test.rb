a0ee5f3a4f7089a0ed39db9a03f237639933c5fb
Vaughn <vaughn.weiss@gmail.com>
2020-12-18 11:38:25 -0700

    Controller // Add basic controller tests

---
+++ test/controllers/locations_controller_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class LocationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get locations_url
+    assert_response :success
+  end
+
+  test "should get index with type" do
+    get locations_url, params: { type: "Dungeon" }
+    assert_response :success
+  end
+
+  # how to test empty set if type doesn't exist?
+
+  test "should get show" do
+    get location_url(dungeons(:known_dungeon))
+    assert_response :success
+  end
+
+end
