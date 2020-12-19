f1f2acaf4a357ced69a587c2986e3c84de1ee958
Vaughn <vaughn.weiss@gmail.com>
2020-12-18 18:22:16 -0700

    Update location_flow_test

--- test/integration/location_flow_test.rb
+++ test/integration/location_flow_test.rb
@@ -8,21 +8,40 @@ class LocationFlowTest < ActionDispatch::IntegrationTest
 
   test "can see page title" do
     get "/locations"
-    assert_select "title", "Pen & Paper | Locations"
+    assert_select "title", "Locations | Pen & Paper"
   end
 
   test "can see Dungeons index" do
     get "/dungeons"
+    assert_select "title", "Dungeons | Pen & Paper"
     assert_select "h1", "Dungeons"
   end
 
   test "can see Geographies index" do
     get "/geographies"
+    assert_select "title", "Geographies | Pen & Paper"
     assert_select "h1", "Geographies"
   end
 
   test "can see Municipalities index" do
     get "/municipalities"
+    assert_select "title", "Municipalities | Pen & Paper"
     assert_select "h1", "Municipalities"
   end
+
+  test "can see Add Location" do
+    get "/locations/new"
+    assert_select "title", "Add Location | Pen & Paper"
+  end
+
+  # test "can add location" do
+  #   get "/locations"
+  #   click_on "+ Add a New Location"
+  #   click_on "Dungeon"
+  #   fill_in "Name", with: "Demo Dungeon"
+  #   fill_in "Description", with: "A very ephemeral dungeon"
+  #   click_on "Visited"
+  #   click_on "Submit"
+  #   assert_select "title", "Demo Dungeon | Pen & Paper"
+  # end
 end
